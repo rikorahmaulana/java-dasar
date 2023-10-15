7c78e8e9e962feab564a1727161fea43833684fe
rik <riko.rmaulana2812@gmail.com>
2023-10-15 21:47:43 +0700

    komit pertama

---
+++ src/TipeDataBukanPrimitif.java
@@ -0,0 +1,34 @@
+public class TipeDataBukanPrimitif {
+    public static void main(String[] args) {
+
+        Integer iniInteger = 100;
+        Long iniLong = 10000L;
+
+        Byte iniByte = null;
+
+        System.out.println(iniByte);
+
+        iniByte = 100;
+
+        System.out.println(iniByte);
+
+        int iniInt = 100;
+
+        Integer iniObject = iniInt;
+
+        short iniShort = iniObject.shortValue();
+        long iniLong2 = iniObject.longValue();
+        float iniFloat = iniObject.floatValue();
+
+        Long amount = 1000000L;
+
+        System.out.println(iniInteger);
+        System.out.println(iniLong);
+        System.out.println(iniByte);
+        System.out.println(iniShort);
+        System.out.println(iniLong);
+        System.out.println(iniLong2);
+        System.out.println(iniFloat);
+        System.out.println(amount);
+    }
+}
