3e7a5c097d6b5ac73cd7057a89119199f14cf461
rik <riko.rmaulana2812@gmail.com>
2023-10-20 21:06:14 +0700

    feat method

---
+++ src/Method.java
@@ -0,0 +1,15 @@
+public class Method {
+    public static void main(String[] args) {
+
+        sayHelloWorld();
+
+        sayHelloWorld();
+
+    }
+
+    static void sayHelloWorld() {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!!");
+    }
+}
