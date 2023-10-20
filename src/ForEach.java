83fd0bb17a5653f87f0931a17c440ca6248b2170
rik <riko.rmaulana2812@gmail.com>
2023-10-20 21:00:01 +0700

    feat for each

---
+++ src/ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] array = {
+                "Riko", "Rahma", "Maualana",
+        };
+
+        for (String value : array) {
+            System.out.println(value);
+        }
+    }
+}
